97f99d15f6adc41a853f60f48c10e447923118db
henri <henri@host.docker.internal>
2020-06-28 17:56:54 -0300

    Novo

---
+++ src/main/java/br/com/erudio/restcurso/MainRest.java
@@ -0,0 +1,13 @@
+package br.com.erudio.restcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainRest {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainRest.class, args);
+	}
+
+}
